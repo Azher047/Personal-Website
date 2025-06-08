0d91619ec19ababcc8a19f8bd81ea5a6c36b0562
Azher047 <77622397+Azher047@users.noreply.github.com>
2025-06-09 00:29:02 +0530

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,16 +60,16 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Azher Hussain.</h2>;
+  const three = <h3 className="big-heading">I build web apps to for PE/VC.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
+        I’m a product manager specialising in building excellent
+        digital experiences. Currently, I’m focused on the next best deal-sourcing platform in the market
         at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        <a href="https://gain.pro" target="_blank" rel="noreferrer">
+          Gain.pro
         </a>
         .
       </p>
