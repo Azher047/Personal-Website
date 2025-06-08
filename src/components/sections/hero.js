25cfef34bd3b0b5be81648d5a89782cb3a96ccbd
Azher047 <77622397+Azher047@users.noreply.github.com>
2025-06-09 00:46:55 +0530

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -78,10 +78,10 @@ const Hero = () => {
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
+      href="https://drive.google.com/file/d/12FwkyyHkqkPcfO6HvXBIJmMQStVURqZD/view?usp=sharing"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Download my resume!
     </a>
   );
 
