b3dc361d0882d995806ad2e91ceff6307b321d07
Azher047 <77622397+Azher047@users.noreply.github.com>
2025-08-07 02:33:40 +0530

    Hide Other Noteworthy Projects section by removing Projects import and component in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Layout, Hero, About, Jobs, Featured, Projects, Contact } from '@components';
+// Removed the Projects import since the “Other Noteworthy Projects” section is temporarily hidden
+import { Layout, Hero, About, Jobs, Featured, Contact } from '@components';
 
 const StyledMainContainer = styled.main`
   counter-reset: section;
@@ -14,7 +15,7 @@ const IndexPage = ({ location }) => (
       <About />
       <Jobs />
       <Featured />
-      <Projects />
+      {/* The Projects section has been intentionally hidden. Remove this comment and re-add <Projects /> when ready to show it again. */}
       <Contact />
     </StyledMainContainer>
   </Layout>
